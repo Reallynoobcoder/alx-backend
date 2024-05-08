31cdfb7973503abdde2daab70f0f49d4a0ce1524
El imali hamza <elimal.hamza@gmail.com>
2024-05-08 13:54:16 +0000

    task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Basic Lifo caching."""
+from collections import deque
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -10,17 +11,19 @@ class LIFOCache(BaseCaching):
         """Class constructor."""
 
         super().__init__()
+        self.order = deque()
 
     def put(self, key, item):
         """Add item to the cach."""
-
         if key is None or item is None:
             return
 
         self.cache_data[key] = item
+        self.order.append(key)
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discarded_key, _ = self.cache_data.popitem()
+            discarded_key = self.order[-2]
             print("DISCARD: {}".format(discarded_key))
+            del self.cache_data[discarded_key]
 
     def get(self, key):
         """Get item by key."""
