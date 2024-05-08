bd7c69e48ce8734d9d46183caec2809cfcdcb4e5
El imali hamza <elimal.hamza@gmail.com>
2024-05-08 17:33:33 +0000

    .

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from collections import OrderedDict
 """class LRUCache that inherits from BaseCaching."""
 BaseCaching = __import__('base_caching').BaseCaching
 
@@ -8,8 +9,8 @@ class LRUCache(BaseCaching):
 
     def __init__(self):
         """Class constructor."""
-
         super().__init__()
+        self.order = OrderedDict()
 
     def put(self, key, item):
         """Add item to the cache."""
@@ -17,16 +18,20 @@ class LRUCache(BaseCaching):
             return
 
         if key in self.cache_data:
-            self.cache_data.pop(key)
+            self.cache_data[key] = item
+            self.order.move_to_end(key)
         elif len(self.cache_data) >= self.MAX_ITEMS:
-            discarded_key = next(iter(self.cache_data))
+            discarded_key = next(iter(self.order))
             self.cache_data.pop(discarded_key)
+            self.order.popitem(last=False)
             print(f"DISCARD: {discarded_key}")
 
         self.cache_data[key] = item
+        self.order[key] = None
 
     def get(self, key):
         """Get item by key."""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        if key in self.cache_data:
+            self.order.move_to_end(key)
+            return self.cache_data[key]
+        return None
